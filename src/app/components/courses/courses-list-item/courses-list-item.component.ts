ac6bdb8c1077d1b765f611641a94320cac7b0980
Yevheniia Bondarieva <12056876-a309428@users.noreply.gitlab.com>
2023-05-17 12:29:27 +0300

    homework_1

---
+++ src/app/components/courses/courses-list-item/courses-list-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-courses-list-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './courses-list-item.component.html',
+  styleUrls: ['./courses-list-item.component.css']
+})
+export class CoursesListItemComponent {
+
+}
---
+++ src/app/components/courses/courses-new-item/courses-new-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-courses-new-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './courses-new-item.component.html',
+  styleUrls: ['./courses-new-item.component.css']
+})
+export class CoursesNewItemComponent {
+
+}
