fafc26f045334d22aefa850be67cbfe4da6c372f
Yevheniia Bondarieva <12056876-a309428@users.noreply.gitlab.com>
2023-05-20 18:22:39 +0300

    homework1_fix2

---
+++ src/app/components/courses-page/courses-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-courses-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './courses-page.component.html',
+  styleUrls: ['./courses-page.component.css']
+})
+export class CoursesPageComponent {
+
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,17 +1,3 @@
 import { Route } from '@angular/router';
 
-import { CoursesListItemComponent } from './components/courses/courses-list-item/courses-list-item.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './components/login/login.component';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-
-export const routes: Route[] = [
-  {
-    path: '',
-    component: HeaderComponent,
-  },
-  {
-    path: ':id',
-    component: CoursesListItemComponent,
-  }
-];
+export const routes: Route[] = [];
